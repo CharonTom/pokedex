ba7746364724d8f0a3665af414bac29b1d9eb6a3
Charon <charon.s.tom@gmail.com>
2025-03-31 10:50:31 +0200

    colors is well implemented

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,5 +1,4 @@
 <script setup>
-import { defineProps, defineEmits } from "vue";
 import { useRouter } from "vue-router";
 import { computed } from "vue";
 import "primeicons/primeicons.css";
@@ -33,22 +32,65 @@ const seeDetails = () => {
 };
 
 const typeColors = {
+  fire: "bg-red-200 text-black rounded-xl",
+  water: "bg-blue-200 text-black rounded-xl",
+  grass: "bg-green-200 text-black rounded-xl",
+  electric: "bg-yellow-200 text-black rounded-xl",
+  poison: "bg-purple-200 text-black rounded-xl",
+  normal: "bg-gray-200 text-black rounded-xl",
+  fighting: "bg-red-200 text-black rounded-xl",
+  flying: "bg-indigo-200 text-black rounded-xl",
+  ground: "bg-yellow-200 text-black rounded-xl",
+  rock: "bg-gray-200 text-black rounded-xl",
+  bug: "bg-lime-200 text-black rounded-xl",
+  ghost: "bg-indigo-200 text-black rounded-xl",
+  steel: "bg-gray-200 text-black rounded-xl",
+  ice: "bg-cyan-200 text-black rounded-xl",
+  dragon: "bg-indigo-200 text-black rounded-xl",
+  dark: "bg-gray-200 text-black rounded-xl",
+  fairy: "bg-pink-200 text-black rounded-xl",
+  psychic: "bg-pink-200 text-black rounded-xl",
+};
+
+const typeTagColors = {
   fire: "bg-red-500 text-white rounded-xl",
   water: "bg-blue-500 text-white rounded-xl",
   grass: "bg-green-500 text-white rounded-xl",
-  electric: "bg-yellow-500 text-black rounded-xl",
+  electric: "bg-yellow-500 text-white rounded-xl",
   poison: "bg-purple-500 text-white rounded-xl",
-  // Ajoute d'autres types selon tes besoins
+  normal: "bg-gray-500 text-white rounded-xl",
+  fighting: "bg-red-500 text-white rounded-xl",
+  flying: "bg-indigo-500 text-white rounded-xl",
+  ground: "bg-yellow-500 text-white rounded-xl",
+  rock: "bg-gray-500 text-white rounded-xl",
+  bug: "bg-lime-500 text-white rounded-xl",
+  ghost: "bg-indigo-500 text-white rounded-xl",
+  steel: "bg-gray-500 text-white rounded-xl",
+  ice: "bg-cyan-500 text-white rounded-xl",
+  dragon: "bg-indigo-500 text-white rounded-xl",
+  dark: "bg-gray-500 text-white rounded-xl",
+  fairy: "bg-pink-500 text-white rounded-xl",
+  psychic: "bg-pink-500 text-white rounded-xl",
 };
 
 const getTypeClass = (typeName) => {
-  return typeColors[typeName] || "bg-gray-500 text-white rounded-xl";
+  return typeTagColors[typeName];
+};
+
+const getCardBackground = (pokemon) => {
+  if (pokemon.types && pokemon.types.length > 0) {
+    const firstType = pokemon.types[0].type.name;
+    return typeColors[firstType];
+  }
 };
 </script>
 
 <template>
   <div
-    class="bg-pink-200 w-62 p-4 border rounded-xl shadow hover:shadow-lg transition relative"
+    :class="[
+      getCardBackground(pokemon),
+      'w-62 p-4 border rounded-xl shadow hover:shadow-lg transition relative',
+    ]"
   >
     <div class="absolute top-2 right-2 flex gap-2">
       <i
@@ -94,7 +136,7 @@ const getTypeClass = (typeName) => {
       </span>
     </div>
     <div class="flex mb-2 gap-x-4 justify-start">
-      <span>{{ pokemon.height / 10 }} mètres</span>
+      <span>{{ pokemon.height / 10 }} meters</span>
       <span>{{ pokemon.weight / 10 }} kg</span>
     </div>
 
